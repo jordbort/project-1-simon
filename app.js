b86318ee26cf725e72cd6eaadeb7b9736b8c04b0
jordbort <jordan.bortner@gmail.com>
2022-11-17 15:47:45 -0500

    Tidy up JavaScript

--- app.js
+++ app.js
@@ -31,33 +31,6 @@ let listening = false
 let selection
 let sequence = []
 
-const newAnswer = () => {
-    theAnswer = []
-    for(i=0;i<5;i++) {
-        theAnswer.push(Math.ceil(Math.random() * 9))
-    }
-    console.log(`The answer is: ${theAnswer}`)
-}
-
-function computerTurn() {
-    console.log(`Round ${roundNumber}: It's the computer's turn!`)
-    playerArr = []
-    sequence = []
-    for(i=0;i<roundNumber;i++) {
-        sequence.push(theAnswer[i])
-        console.log(`*** Building round sequence: ${sequence.length} / ${roundNumber}`)
-    }
-    enableButtons()
-    console.log(`It's your turn! Round sequence is: ${sequence}`)
-}
-
-function reset() {
-    console.log(`*** GAME RESET`)
-    newAnswer()
-    roundNumber = 1
-    computerTurn()
-}
-
 // #626162
 const disableButtons = () => {
     listening = false
@@ -68,29 +41,69 @@ const enableButtons = () => {
     console.log(`*** Buttons enabled`)
 }
 
-// function wrongAnswer() {}
+const newAnswer = () => {
+    theAnswer = []
+    for(i=0;i<5;i++) {
+        theAnswer.push(Math.ceil(Math.random() * 9))
+    }
+    console.log(`The answer is: ${theAnswer}`)
+}
 
-function win() {
+const computerTurn = () => {
+    console.log(`Round ${roundNumber}: It's the computer's turn!`)
+    // disableButtons()
+    sequence = []
+    for(i=0;i<roundNumber;i++) {
+        sequence.push(theAnswer[i])
+        console.log(`*** Building round sequence: ${sequence.length} / ${roundNumber}`)
+    }
+    // this is where the computer should blink the sequence at the player
+    playerArr = []
+    enableButtons()
+    console.log(`It's your turn! Round sequence is: ${sequence}`)
+}
+
+const reset = () => {
+    console.log(`*** GAME RESET`)
+    newAnswer()
+    roundNumber = 1
+    computerTurn()
+}
+
+const wrongClick = () => {
+    // instead of disableButtons() there will be an "all boxes red" function
+    disableButtons()
+    reset()
+}
+
+const win = () => {
     console.log(`Task Completed! GAME OVER`)
+    // instead of disableButtons() there will be an "all boxes blue" function
+    disableButtons()
     theAnswer = []
 }
 
-function checkSequence() {
-    console.log(`*** Round ${roundNumber} complete!`)
-    roundNumber++
-    disableButtons()
-    if(roundNumber > 5) {
-        win()
-    }
-    else {
-        computerTurn()
-    }
-}
+// const checkSequence = () => {
+//     if(roundNumber > 5) {
+//         win()
+//     }
+//     else {
+//         computerTurn()
+//     }
+// }
 
-function submitAnswer() {
+const buildPlayerArr = () => {
     console.log(`Round progress: ${playerArr.length} / ${roundNumber}. Sequence: ${sequence}`)
     if(playerArr.length === roundNumber) {
-        checkSequence()
+        console.log(`*** Round ${roundNumber} complete!`)
+        roundNumber++
+        // checkSequence()
+        if(roundNumber > 5) {
+            win()
+        }
+        else {
+            computerTurn()
+        }
     }
 }
 
@@ -102,16 +115,15 @@ rightBoxes.forEach(button => {
             if(selection === theAnswer[playerArr.length]) {
                 playerArr.push(selection)
                 console.log(`***`, selection, `was correct! Your array: ${playerArr}`)
+                buildPlayerArr()
                 this.style.backgroundColor = `#327DBD`
                 setTimeout(() => this.style.backgroundColor = `#B2B0B3`, 100)
-                submitAnswer()
             }
             else {
                 console.log(`***`, selection, `was NOT correct!`)
-                // wrongClick() will replace the color changes below
+                wrongClick()
                 this.style.backgroundColor = `#D12F00`
                 setTimeout(() => this.style.backgroundColor = `#B2B0B3`, 100)
-                reset()
             }
         }
     })
